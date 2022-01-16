3ea2bd40d53055d334565a2b113338b90948b7de
Bon Leif Amalla <bonleifamalla@gmail.com>
2022-01-16 21:12:12 +0900

    Add callback getting rid of COM motion

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -16,5 +16,48 @@ module Callbacks
 
 import ..MDPlaygroundCore: AbstractCallback, AbstractState
 
+abstract type FixedIntervalCallback <: AbstractCallback end
+
+struct COMCallback <: FixedIntervalCallback
+    steps::Int
+    uniform::Bool
+end
+function (cb::COMCallback)(currstep, st)
+    # get average momentum and get rid of it
+    # so that center of mass does not move
+    currstep % cb.steps == 0 && return
+
+    if cb.uniform
+        _uniformCOM!(st)
+    else
+        _nonuniformCOM!(st)
+    end
+end
+function _uniformCOM!(st::AbstractState)
+    ## get mean momentum
+    ## subtract from everything
+    len = length(st)
+    μmom = sum(conf->conf[:p], config) / len
+
+    for i in eachindex(st.config)
+        @inbounds st.config[i][:p] = st.config[i][:p] .- μmom
+    end
+end
+function _nonuniformCOM!(st::AbstractState)
+    # get mean velocity (not weighted)
+    # subtract mass * velocity from current momenta
+    len = length(st)
+    ps = (conf[:p] for conf in st.config)
+    masses = (particle.mass for particle in st.particles)
+    
+    μv = sum(zip(ps, masses)) do (p, m)
+        p / m
+    end / len
+
+    for (i, m) in enumerate(masses)
+        @inbounds st.config[i][:p] = st.config[i][:p] .- μv * m
+    end
+end
+
 end ### module
 
