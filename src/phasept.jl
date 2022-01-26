e571116c6d7f7422318885b53f7fce5b35ba8c8e
Bon Leif Amalla <bonleifamalla@gmail.com>
2022-01-26 11:28:34 +0900

    Use an SVector partition
    Changing/accessing using symbols should be the same

--- src/phasept.jl
+++ src/phasept.jl
@@ -12,70 +12,26 @@ function dim end
 
 ## Phase space points definition
 
-mutable struct Phase2D{T}
-    x::T
-    y::T
-    px::T
-    py::T
-end
-dim(::Type{Phase2D}) = 2
-dim(p::Phase2D) = dim(typeof(p))
+"""
+    PhasePartition{N,Tx,Tp}
 
-mutable struct Phase3D{T}
-    x::T
-    y::T
-    z::T
-    px::T
-    py::T
-    pz::T
-end
-dim(::Type{Phase3D}) = 3
-dim(p::Phase3D) = dim(typeof(p))
-
-## TODO: set up constructors here
-
-function Phase2D(x, y, px, py)
-    T = promote_type(typeof(x), typeof(y),
-                     typeof(px), typeof(py))
-    @assert T <: Real ### make sure there are no units
-    return Phase2D{T}(x, y, px, py)
+A representation of phase space points. `:x` contains position, while `:p`
+contains momentum for a particle. Contains SVectors for both fields of the
+same length `N`. Position has `eltype` `Tx` and momentum has `eltype` `Tp`.
+Units can be supported (note operator output dimensions).
+"""
+mutable struct PhasePartition{N,Tx,Tp}
+    x::SVector{N,Tx}
+    p::SVector{N,Tp}
 end
 
-function Phase3D(x, y, z, px, py, pz)
-    T = promote_type(typeof(x), typeof(y), typeof(z),
-                     typeof(px), typeof(py), typeof(pz))
-    @assert T <: Real ### make sure there are no units
-    return Phase3D{T}(x, y, z, px, py, pz)
-end
+dim(::Type{PhasePartition{N}}) where N = N
+dim(ppart::PhasePartition) = dim(typeof(ppart))
 
 ## accessors and mutators
 
-const Phase{T} = Union{Phase2D{T}, Phase3D{T}}
-
-@generated function Base.getindex(phase::Phase{T}, s::Symbol) where T
-    d = dim(phase)
-    return quote
-        offset = _getpartition($d, s)
-        return SVector{$d,T}(_solvepartition(phase, offset))
-    end
-end
-@generated function Base.setindex!(phase::Phase{T}, val, s::Symbol) where T
-    d = dim(phase)
-    extra = d == 3 ? :(setfield!(phase, offset+3, T(val[3]))) : :()
-    return quote
-        offset = _getpartition($d, s)
-        setfield!(phase, offset+1, T(val[1]))
-        setfield!(phase, offset+2, T(val[2]))
-        $extra
-    end
-end
-
-function _solvepartition(phase::Phase, offset)
-    ntuple(i->getfield(phase, offset+i), dim(phase))
-end
-function _getpartition(d, s)
-    (s == :x || s == :q) && return 0
-    s == :p || throw(ArgumentError("Cannot access symbol $s."))
-    return d
+Base.getindex(ppart::PhasePartition, s::Symbol) = getfield(ppart, s)
+function Base.setindex!(ppart::PhasePartition{N}, val, s::Symbol)
+    setfield!(ppart, s, SVector{N}(val))
 end
 
